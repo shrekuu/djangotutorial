d62ddf47e6699d93a02a1321915b06664cb5ccfc
Bond <shrekuu@gmail.com>
2025-04-02 15:29:38 +0800

    redirect landing page to /polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    # redirect / to /polls
+    path("", lambda request: HttpResponseRedirect("/polls")),
+
     path("polls", include("polls.urls")),
     path('admin', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
